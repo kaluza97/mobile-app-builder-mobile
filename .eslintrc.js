0326cd1bca2dba2d2f9327920d135083ba4f0e4c
kaluza97 <piotrkaluzny@MacBook-Pro-Piotr.local>
2024-09-29 21:44:45 +0200

    add eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    '@react-native',
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+  ],
 };
